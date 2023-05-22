9b16f0d5552fe4ecc9fc7cd45934e9964e7ba467
bramba2000 <bramba2000@gmail.com>
2023-05-22 23:03:51 +0200

    add UI theme in common files

---
+++ sl_common/lib/theme/sl_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SLKitchenTheme {
+  static get lightTheme => ThemeData(
+        colorSchemeSeed: const Color(0xFFB65739),
+        textTheme: TextTheme(),
+        useMaterial3: true,
+      );
+}
