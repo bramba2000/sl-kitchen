14afcf06a4aee2623bf38537487a540549a0f68a
bramba2000 <bramba2000@gmail.com>
2023-06-26 11:23:22 +0200

    add header section

--- sl_front/lib/main.dart
+++ sl_front/lib/main.dart
@@ -24,9 +24,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text('S.Luigi Kitchen',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+        toolbarHeight: 70.0,
+        backgroundColor: Colors.white,
+        title: const _Header(),
         actions: [
           IconButton(
             icon: Icon(
@@ -41,5 +41,56 @@ class HomePage extends StatelessWidget {
         child: Text('Hello Worlds!'),
       ),
     );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          width: 45,
+          height: 51.43,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/icons/logo.jpg'),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        const Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'SAN LUIGI ',
+                style: TextStyle(
+                  color: Color(0xFFC8AA87),
+                  fontSize: 24,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              TextSpan(
+                text: 'KITCHEN',
+                style: TextStyle(
+                  color: Color(0xFF616068),
+                  fontSize: 24,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+    ;
   }
 }
