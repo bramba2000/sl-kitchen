b2ef51a69b16708d20d74539eb97148a9a88e972
bramba2000 <bramba2000@gmail.com>
2023-06-26 15:53:35 +0200

    add gray as background color

--- sl_front/lib/themes/light_theme.dart
+++ sl_front/lib/themes/light_theme.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class SLKTheme {
   static const primaryColor = Color(0xFFC8AA87);
   static const accentColor = Color(0xFF616068);
+  static const backgroundColor = Color(0xFFEDEDED);
 
   static ThemeData get ligthTheme =>
       ThemeData.light(useMaterial3: true).copyWith(
-          colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+          colorScheme: ColorScheme.fromSeed(seedColor: primaryColor)
+              .copyWith(background: backgroundColor),
           elevatedButtonTheme: ElevatedButtonThemeData(
               style: ElevatedButton.styleFrom(
                   shape: RoundedRectangleBorder(
